90a82fb3b263ce9d53c73adc619c53c8d39ada0a
Siren K <siren.k.pro@gmail.com>
2021-01-01 15:44:31 +0900

    begin to study functional programming with go lang

--- cmd/ch01/01_oop/main.go
+++ cmd/ch01/01_oop/main.go
@@ -5,6 +5,8 @@ import (
 	. "github.com/siren-k/study-learning-functional-programming-in-go/cmd/ch01/01_oop/internal"
 )
 
+// 본질적으로 명령행 코드와 동일한 연산을 수행한다. 프로그램의 상태를 객체의 속성에 배정하고,
+// 메소드를 호출해 내부 상태를 수정하고, 원하는 결과에 도달할 때까지 실행 상태를 변경한다.
 func main() {
 	MyCars.Add(Car{"IS250"})
 	MyCars.Add(Car{"Blazer"})
