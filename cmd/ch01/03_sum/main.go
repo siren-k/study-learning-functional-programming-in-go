7a4c8b8bbc668970c86ecdb5c98aba2954381217
Siren K <siren.k.pro@gmail.com>
2021-01-01 19:58:04 +0900

    Examples of instruction and functional functions

--- cmd/ch01/03_sum/main.go
+++ cmd/ch01/03_sum/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	. "github.com/siren-k/study-learning-functional-programming-in-go/cmd/ch01/03_sum/internal"
+)
 
+func main() {
+	fmt.Println(SumLoop([]int{1, 2, 3}))
+	fmt.Println(SumRecursive([]int{1, 2, 3}))
 }
